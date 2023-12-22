6f51a28efa88507bdda81e9996d4a6f5dd0ca962
Elias <75465281+eliskol@users.noreply.github.com>
2023-12-22 00:17:17 -0800

    checks are persistent

--- scripts/module-pins.js
+++ scripts/module-pins.js
@@ -1,21 +1,43 @@
 console.log("Hi!");
 
-function main() {
+let pins = [];
+
+async function mainFunc() {
     let moduleItems = document.querySelectorAll("li.context_module_item:not(#context_module_item_blank)");
     console.log(`${moduleItems.length} Module Items!!`);
 
+    // grab pins
+    await chrome.storage.local.get("pins").then((result) => {
+        pins = result.pins;
+        console.log(pins);
+    });
+
+    // add pin checkbox to each module item
     moduleItems.forEach(listItem => {
         let containerDiv = listItem.querySelector(".ig-row");
         let pinCheckbox = document.createElement("input");
         pinCheckbox.type = "checkbox";
-        pinCheckbox.addEventListener("click", () => {
+        pinCheckbox.addEventListener("click", async () => {
             if (pinCheckbox.checked) {
-                moveToTop(listItem);
+                // moveToTop(listItem);
+                if (!pins.includes(listItem.id)){
+                    pins.push(listItem.id);
+                    await chrome.storage.local.set({ "pins": pins }).then(() => {
+                        console.log("Updated pins");
+                    });
+                    await chrome.storage.local.get("pins").then((result) => {
+                        console.log("Value currently is " + JSON.stringify(result));
+                    });
+                }
             }
             else {
 
             }
-        })
+        });
+        if (pins.includes(listItem.id)) {
+            pinCheckbox.checked = true;
+            console.log("checkd");
+        }
         containerDiv.appendChild(pinCheckbox);
     });
 
@@ -27,5 +49,5 @@ function moveToTop(moduleItem) {
 }
 
 setTimeout(() => {
-    main();
+    mainFunc();
 }, 750);
