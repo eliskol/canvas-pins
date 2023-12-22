39440c95b85eaf5007f140994251e89c9bdf7b60
Elias <75465281+eliskol@users.noreply.github.com>
2023-12-22 00:51:24 -0800

    it works! just need to make pins list update correctly after removing pin, and be able to unpin from pins module

--- scripts/module-pins.js
+++ scripts/module-pins.js
@@ -1,6 +1,5 @@
-console.log("Hi!");
-
 let pins = [];
+let pinsModule;
 
 async function mainFunc() {
     let moduleItems = document.querySelectorAll("li.context_module_item:not(#context_module_item_blank)");
@@ -12,27 +11,34 @@ async function mainFunc() {
         console.log(pins);
     });
 
+    if (pins != []) {
+        pinsModule = createPinsModule()
+        moveToTop(pinsModule);
+    }
+
     // add pin checkbox to each module item
     moduleItems.forEach(listItem => {
         let containerDiv = listItem.querySelector(".ig-row");
         let pinCheckbox = document.createElement("input");
         pinCheckbox.type = "checkbox";
         pinCheckbox.addEventListener("click", async () => {
-            if (pinCheckbox.checked) {
-                // moveToTop(listItem);
-                if (!pins.includes(listItem.id)){
-                    pins.push(listItem.id);
-                    await chrome.storage.local.set({ "pins": pins }).then(() => {
-                        console.log("Updated pins");
-                    });
-                    await chrome.storage.local.get("pins").then((result) => {
-                        console.log("Value currently is " + JSON.stringify(result));
-                    });
-                }
+            await chrome.storage.local.get("pins").then((result) => {
+                console.log("Value currently is " + JSON.stringify(result));
+                pins = result.pins
+            });
+            if (pinCheckbox.checked && !pins.includes(listItem.id)) {
+                pins.push(listItem.id);
             }
-            else {
-
+            else if (!pinCheckbox.checked && pins.includes(listItem.id)) {
+                pins.pop(pins.indexOf(listItem.id));
             }
+            await chrome.storage.local.set({ "pins": pins }).then(() => {
+                console.log("Updated pins");
+            });
+            await chrome.storage.local.get("pins").then((result) => {
+                console.log("Value currently is " + JSON.stringify(result));
+            });
+            updatePinsModule(pins);
         });
         if (pins.includes(listItem.id)) {
             pinCheckbox.checked = true;
@@ -41,6 +47,8 @@ async function mainFunc() {
         containerDiv.appendChild(pinCheckbox);
     });
 
+    updatePinsModule(pins);
+
 }
 
 function moveToTop(moduleItem) {
@@ -48,6 +56,38 @@ function moveToTop(moduleItem) {
     moduleList.prepend(moduleItem);
 }
 
+function createPinsModule() {
+    let pinsModule = document.createElement("div");
+    pinsModule.classList.add("context_module");
+    pinsModule.classList.add("item-group-condensed");
+
+    let headerDiv = document.createElement("div");
+    headerDiv.classList.add("ig-header");
+    headerDiv.classList.add("header");
+
+    let headerSpan = document.createElement("span");
+    headerSpan.classList.add("ig-header-title");
+    headerSpan.innerText = "Pins";
+
+    let contentDiv = document.createElement("div");
+    contentDiv.classList.add("content");
+
+    headerDiv.appendChild(headerSpan);
+    pinsModule.appendChild(headerDiv);
+    pinsModule.appendChild(contentDiv);
+
+    return pinsModule;
+}
+
+function updatePinsModule(pins) {
+    let contentDiv = pinsModule.querySelector("div.content");
+    contentDiv.innerHTML = "";
+    pins.forEach(listItemId => {
+        let listItem = document.querySelector(`#${listItemId}`);
+        contentDiv.appendChild(listItem.cloneNode(true));
+    });
+}
+
 setTimeout(() => {
     mainFunc();
-}, 750);
+}, 500);
